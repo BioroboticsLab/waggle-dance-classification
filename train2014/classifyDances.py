7b396e005a6fba67824fcbcb153367cb2c3caee5
Mehmed Halilovic <mehmed.h@hotmail.de>
2016-03-19 13:48:41 +0100

    Started refactoring classifyDances.py and testDanceRecognitionExtractWrong.py

--- train2014/classifyDances.py
+++ train2014/classifyDances.py
@@ -1,16 +1,27 @@
+"""
+Classifies the dances of the results of the WDD with the given weights and the neural network in kerasModel.py.
+Every positive dance gets copied into the result folder.
+Usage: python classify Dances. py <wddOutputRoot> <inputModelFile> <resultFolder>
+:param wddOutputRoot: root of the folder with the unfiltered dances.
+:param inputModelFile: trained weights of the neural network in kerasModel.py
+:param resultFolder: destination of the positively classified dances
+"""
 import numpy as np
 import os
 import glob
 import scipy
-import csv
-import matplotlib.pyplot as plt
 import kerasModel
+import sys
 
-from keras.utils import np_utils
 from shutil import copytree
 
 
 def classify_dance(img_array):
+    """
+    Uses the neural network to give predictions for every single window of a dance.
+    :param img_array:
+    :return:
+    """
     x_verify = []
     for i in range(img_array.shape[0]-30):
         x_verify.append(img_array[i:i+30,:,:])
@@ -21,6 +32,14 @@ def classify_dance(img_array):
 
 
 def update_confusion_matrix(predictions, CM, dirName):
+    """
+    Updates the confusion matrix with the predictions of the windows of one dance, by using border 0.5.
+    Copies positively classified dances into the result folder.
+    :param predictions: predictions of the dance
+    :param CM: confusion matrix (2,2) that will be updated
+    :param dirName: destination of the positively classified dances.
+    :return: CM: updated confusion matrix
+    """
     predictions_sum = np.sum(predictions, axis=0, dtype="float32")[1]
     mean = predictions_sum/len(predictions)
     border = 0.5
@@ -28,25 +47,33 @@ def update_confusion_matrix(predictions, CM, dirName):
         CM[0] += 1
     else:
         CM[1] += 1
-        copytree(dirName, '/home/mehmed/Desktop/Clean/' + dirName)
+        copytree(dirName, resultFolder + dirName)
     return CM
+
+# Get input arguments
+wddOutputRoot = sys.argv[1]
+inputModelFile = sys.argv[2]
+resultFolder = sys.argv[3]
+
+print 'Folder with unfiltered dances is "', wddOutputRoot
+print 'Input model file is "', inputModelFile
 
 kM = kerasModel.KerasModel()
 model = kM.getModel()
 
-model.load_weights('my_model_weights_0103w.h5')
+model.load_weights(inputModelFile)
 
 # Init Confusion Matrix
-CM = np.zeros((2))
+CM = np.zeros((2,2))
 progress = 0
-# traverse folder structure and build matrix for every single dance so that CNN can test it
+# Traverse folder structure and build matrix for every single dance so that CNN can test it
 # Set the directory you want to start from
-rootDir = '/home/mehmed/Desktop/20140822'
+rootDir = wddOutputRoot
 for dirName, subdirList, fileList in os.walk(rootDir):
     if 'orient.png' in fileList:
             image_list = []
             for fname in glob.glob(dirName + '/image*.png'):
-                if (fname != 'orient.png'):
+                if fname != 'orient.png':
                     im = scipy.misc.imread(fname)[:, :, 1]
                     image_list.append(im)
             image_array = np.asarray(image_list)
--- train2014/testDanceRecognitionExtractWrong.py
+++ train2014/testDanceRecognitionExtractWrong.py
@@ -1,16 +1,29 @@
+"""
+Given a folder with Ground Truth Data, this script checks if the neural network classifies every dance correctly.
+If not the dance is copied to a folder in given location. There will be two folders, one for false positives and one
+for false negatives.
+Usage: python testDanceRecognitionExtractWrong.py <validationFolder> <inputModelFile> <outputFolder>
+:param validationFolder: The root of the folder with the Ground Truth data that should be used for validation
+:param inputModelFile: The weights that should be tested for the neural network
+:param outputFolder: The folder in which the wrong classifications will be saved.
+"""
 import numpy as np
 import os
 import glob
 import scipy
 import csv
-import matplotlib.pyplot as plt
 import kerasModel
+import sys
 
-from keras.utils import np_utils
 from shutil import copytree
 
 
 def classify_dance(img_array):
+    """
+    Uses the neural network to give predictions for every single window of a dance.
+    :param img_array:
+    :return:
+    """
     x_verify = []
     for i in range(img_array.shape[0]-30):
         x_verify.append(img_array[i:i+30,:,:])
@@ -21,6 +34,14 @@ def classify_dance(img_array):
 
 
 def update_confusion_matrix(predictions, CM, Y, dirName):
+    """
+    Updates the confusion matrix with the predictions of the windows of one dance, by using a border of 0.5.
+    Additionally wrong classifications will be copied into the folder validationFolder.
+    :param predictions: predictions of the dance
+    :param CM: confusion matrix (2,2) that will be updated
+    :param Y: the actual class of the dance (0 or 1)
+    :return: CM: updated confusion matrix
+    """
     predictions_sum = np.sum(predictions, axis=0, dtype="float32")[1]
     mean = predictions_sum/len(predictions)
     border = 0.5
@@ -29,27 +50,34 @@ def update_confusion_matrix(predictions, CM, Y, dirName):
             CM[1, 1] += 1
         elif Y == 1:
             CM[1, 0] += 1
-#	    copytree(dirName, '/home/mehmed/Desktop/Wron2g/BL_Dance' + dirName)
+            copytree(dirName, outputFolder + '/BL_Dance' + dirName)
     else:
         if Y == 0:
             CM[0, 1] += 1
- #           copytree(dirName, '/home/mehmed/Desktop/Wron2g/TR_Dance' + dirName)
+            copytree(dirName, outputFolder + '/TR_Dance' + dirName)
         elif Y == 1:
             CM[0, 0] += 1
     return CM
 
+validationFolder = sys.argv[1]
+inputModelFile = sys.argv[2]
+outputFolder = sys.argv[3]
+
+print 'Validation folder is "', validationFolder
+print 'Input model file is "', inputModelFile
+print 'Output folder is "', outputFolder
+
 kM = kerasModel.KerasModel();
 model = kM.getModel()
 
-model.load_weights('my_model_weights_0103w.h5')
+model.load_weights(inputModelFile)
 
-#Test model
 # Init Confusion Matrix
 CM = np.zeros((2,2))
 progress = 0
 # traverse folder structure and build matrix for every single dance so that CNN can test it
 # Set the directory you want to start from
-rootDir = '/home/mehmed/Desktop/Validation2014'
+rootDir = validationFolder
 for dirName, subdirList, fileList in os.walk(rootDir):
     print('Found directory: %s' % dirName)
     if 'gt.csv' in fileList:
